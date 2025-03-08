cab2ef4e2bdac3f3e991357ef219e022819a337e
Fern Garden <mail@fern.garden>
2025-03-08 11:37:41 +0800

    Revert "Add ssh known hosts"
    This reverts commit 422968232410a95c800196d01c2af6392a3fe6ae.

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -43,44 +43,6 @@ in
         AllowUsers = [ config.${namespace}.user.name ];
       };
       authorizedKeysInHomedir = false;
-      knownHosts = {
-        "spoonbill" = {
-          hostNames = [
-            "spoonbill.internal"
-            "10.0.1.2"
-          ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLhv0WaxWuQhBb3BG4wrebkb+egB2hdeysbODTGXSSQ";
-        };
-
-        "weebill" = {
-          hostNames = [
-            "weebill.internal"
-            "10.0.1.3"
-          ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOltBRuLQ7MOZK8T1aYUKdBHXcshNPv+/EMoC7lXsE7";
-        };
-
-        "musk-duck" = {
-          hostNames = [
-            "musk-duck.internal"
-            "10.0.1.10"
-          ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEp5zVloqXFtLEVCl44MwvdkfzIL4MsLqmENXjgPfnQ";
-        };
-
-        "pardalote" = {
-          hostNames = [
-            "pardalote.internal"
-            "10.0.1.12"
-          ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEp5zVloqXFtLEVCl44MwvdkfzIL4MsLqmENXjgPfnQ";
-        };
-
-        "github" = {
-          hostNames = [ "github.com" ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-        };
-      };
       hostKeys = [
         {
           path = "/etc/ssh/ssh_host_ed25519_key";
