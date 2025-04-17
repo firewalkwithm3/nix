a37e3abfd80cba7195f805ab4642a169ba72e3da
Fern Garden <mail@fern.garden>
2025-04-18 00:34:13 +0800

    Second MC server

--- modules/nixos/services/minecraft/default.nix
+++ modules/nixos/services/minecraft/default.nix
@@ -2,6 +2,7 @@
   inputs,
   config,
   lib,
+  pkgs,
   namespace,
   ...
 }:
@@ -9,6 +10,26 @@ with lib;
 with lib.${namespace};
 let
   cfg = config.${namespace}.services.minecraft;
+
+  globalConfig = {
+    EULA = "TRUE";
+    TZ = "Australia/Perth";
+    TYPE = "PAPER";
+    ONLINE_MODE = "FALSE";
+    MEMORY = "8G";
+    DIFFICULTY = "normal";
+    MAX_PLAYERS = "10";
+    SNOOPER_ENABLED = "false";
+    SPAWN_PROTECTION = "0";
+    PVP = "false";
+    VIEW_DISTANCE = "24";
+    SIMULATION_DISTANCE = "12";
+    ENABLE_WHITELIST = "true";
+    # OVERRIDE_WHITELIST = "true";
+    USE_AIKAR_FLAGS = "true";
+    ENABLE_RCON = "false";
+
+  };
 in
 {
   options.${namespace}.services.minecraft = with types; {
@@ -17,37 +38,40 @@ in
   };
 
   config = mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = [ cfg.port ];
+    networking.firewall.allowedTCPPorts = [
+      cfg.port
+    ];
 
     age.secrets.minecraft.rekeyFile = (inputs.self + "/secrets/services/minecraft.age");
 
     virtualisation.oci-containers.containers = {
-      minecraft = {
-        image = "itzg/minecraft-server";
+      velocity = {
+        image = "itzg/mc-proxy";
         ports = [ "${toString cfg.port}:${toString cfg.port}" ];
+        volumes = [
+          "velocity-server:/server"
+          "velocity-config:/config"
+        ];
+        environment = {
+          TYPE = "VELOCITY";
+          ICON = "https://raw.githubusercontent.com/firewalkwithm3/nix/refs/heads/main/packages/www-transgender-pet/images/server-icon.png";
+          OVERRIDE_ICON = "TRUE";
+        };
+        extraOptions = [
+          "--pull=newer"
+        ];
+      };
+
+      minecraft-main = {
+        image = "itzg/minecraft-server";
         volumes = [
           "minecraft:/data"
         ];
+        dependsOn = [ "velocity" ];
         environmentFiles = [ config.age.secrets.minecraft.path ];
         environment = {
-          EULA = "TRUE";
-          TZ = "Australia/Perth";
-          TYPE = "PAPER";
-          VERSION = "LATEST";
-          MEMORY = "10G";
-          MOTD = "meow";
-          DIFFICULTY = "hard";
-          ICON = "https://raw.githubusercontent.com/firewalkwithm3/nix/refs/heads/main/packages/www-transgender-pet/images/server-icon.png";
-          OVERRIDE_ICON = "TRUE";
-          MAX_PLAYERS = "10";
-          SNOOPER_ENABLED = "false";
-          SPAWN_PROTECTION = "0";
-          PVP = "false";
-          VIEW_DISTANCE = "24";
-          SIMULATION_DISTANCE = "12";
-          ENABLE_WHITELIST = "true";
-          OVERRIDE_WHITELIST = "true";
-          USE_AIKAR_FLAGS = "true";
+          SERVER_PORT = "30066";
+          VERSION = "1.21.4";
           MODRINTH_PROJECTS = ''
             essentialsx
             essentialsx-antibuild
@@ -60,17 +84,40 @@ in
             https://dev.bukkit.org/projects/dead-chest/files/latest
           '';
           SPIGET_RESOURCES = "40313"; # ChestCleaner
-        };
+        } // globalConfig;
         extraOptions = [
           "--pull=newer"
           "--tty"
+          "--network=container:velocity"
+        ];
+      };
+
+      minecraft-bob = {
+        image = "itzg/minecraft-server";
+        dependsOn = [ "velocity" ];
+        volumes = [
+          "minecraft-bob:/data"
+        ];
+        environmentFiles = [ config.age.secrets.minecraft.path ];
+        environment = {
+          SERVER_PORT = "30067";
+          VERSION = "1.21.5";
+          PAPER_CHANNEL = "experimental";
+        } // globalConfig;
+        extraOptions = [
+          "--pull=newer"
+          "--tty"
+          "--network=container:velocity"
         ];
       };
     };
 
     ${namespace} = {
       backups.modules.minecraft = {
-        directories = [ "${podmanVolumeDir}/minecraft" ];
+        directories = [
+          "${podmanVolumeDir}/minecraft"
+          "${podmanVolumeDir}/minecraft-bob"
+        ];
       };
     };
   };
